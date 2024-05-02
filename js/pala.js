33221b8007ac3730e285b59e98c27194fcd0784d
AlexBCX <alexxbc1998@gmail.com>
2024-05-02 11:53:45 +0200

    control teclas crack

--- js/pala.js
+++ js/pala.js
@@ -11,9 +11,21 @@ class Pala {
         this.vx = 2;                                                     // velocitat = 10 píxels per fotograma
         this.color = "#D30"; 
     }
-
+    //Moviment pala
     update(){
-       //s
+       if(this.Key.LEFT.pressed){
+        this.posicio.x-=this.vx
+        if(this.posicio.x<0){
+            this.posicio.x=0;
+        }
+    }
+    if(this.Key.RIGHT.pressed){
+        this.posicio.x+=this.vx;
+        if(this.posicio.x>this.amplada)
+        {
+            this.posicio.x=this.amplada;
+        }
+    }
     }
    
     draw(ctx) {
@@ -21,7 +33,6 @@ class Pala {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.posicio.x, this.posicio.y, this.amplada, this.alcada);
         ctx.restore();
-
     }
     mou(x,y){
         this.posicio.x += x;
