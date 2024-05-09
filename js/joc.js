a7169ef9a459c44aa8a24b7bea921d8693044b6f
ecayuelav <ecayuelav@edu.tecnocampus.cat>
2024-05-09 11:30:27 +0200

    muro bonito

--- js/joc.js
+++ js/joc.js
@@ -9,13 +9,16 @@ class Joc{
         this.ctx = ctx;
         this.amplada = canvas.width;
         this.alcada = canvas.height;
+        /*
         this.totxoamplada = 22;
         this.totxoalcada = 10; // MIDES DEL TOTXO EN PÍXELS
         this.totxocolor = 20;
+        */
        
         this.bola = new Bola(new Punt(this.canvas.width/2,this.canvas.height/2),3);
         this.pala = new Pala(new Punt((this.canvas.width-60)/2,this.canvas.height-15),60,4);
-        this.totxo = new Totxo(new Punt((this.canvas.width-120)/2,(this.canvas.height-20)/3), 120, 20, "#0ad");  // només posem un totxo gegant
+        this.mur = new Mur(this.canvas, this.ctx);
+        //this.totxo = new Totxo(new Punt((this.canvas.width-120)/2,(this.canvas.height-20)/3), 120, 20, "#0ad");  // només posem un totxo gegant
 
         this.key = {
             LEFT:{code:37, pressed:false},
@@ -27,7 +30,8 @@ class Joc{
         this.clearCanvas();
         this.pala.draw(this.ctx);
         this.bola.draw(this.ctx);
-        this.totxo.draw(this.ctx);
+        this.mur.draw();
+        //this.totxo.draw(this.ctx);
     }
 
     clearCanvas(){
@@ -37,7 +41,9 @@ class Joc{
     inicialitza(){
         this.pala.draw(this.ctx);
         this.bola.draw(this.ctx);
-        this.totxo.draw(this.ctx);
+        //this.totxo.draw(this.ctx);
+        this.mur.generaMur();
+        this.mur.draw();
         $(document).on("keydown",{joc:this}, function(e){
            //Moviment de la pala
         });
--- js/mur.js
+++ js/mur.js
@@ -4,16 +4,52 @@
 
 class Mur {
     
-    constructor() {
+    constructor(canvas, ctx) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.filaCount = 4; // Nombre de files de totxos
+        this.columnaCount = 10; // Nombre de columnes de totxos
+        this.totxoAmplada; // Amplada de cada totxo
+        this.totxoAlcada; // Alçada de cada totxo
+        this.padding; // Espai entre totxos
+        this.totxos = [];
         
+
+        
+        // Generem els totxos utilitzant la classe Totxo
+        this.generaMur();
     }
 
     generaMur(){
-       
+        this.offsetTop = 5;
+        this.offsetLeft = 6;
+        //this.totxoAmplada = this.canvas.width / this.columnaCount -4;
+        console.log(joc.amplada);
+        this.totxoAmplada = (joc.amplada-this.offsetLeft*2-2*this.columnaCount) / this.columnaCount;
+        this.totxoAlcada = joc.alcada / (this.filaCount * 2+2); // Ajusta este valor según tus necesidades
+        this.padding = (joc.amplada-(this.totxoAmplada*this.columnaCount))/this.columnaCount-1; // Ajusta este valor según tus necesidades
+        
+    
+        for (let c = 0; c < this.columnaCount; c++) {
+            this.totxos[c] = [];
+            for (let r = 0; r < this.filaCount; r++) {
+                const totxoX = (c * (this.totxoAmplada + this.padding)) + this.offsetLeft;
+                const totxoY = (r * (this.totxoAlcada + this.padding)) + this.offsetTop;
+                const puntPosicio = { x: totxoX, y: totxoY };
+                const totxo = new Totxo(puntPosicio, this.totxoAmplada, this.totxoAlcada);
+                this.totxos[c][r] = totxo;
+            }
+        }
     }
+    
 
     draw(ctx){
-       
+        for (let c = 0; c < this.columnaCount; c++) {
+            for (let r = 0; r < this.filaCount; r++) {
+                const totxo = this.totxos[c][r];
+                totxo.draw(this.ctx);
+            }
+        }
     }
      
     defineixNivells(){
