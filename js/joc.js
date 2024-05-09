4e38ff3627cb48f7af20f1d7c7968447dbb9ca26
Noel Sariñena Varela <noelsava25@gmail.com>
2024-05-09 11:23:38 +0200

    Cambios en el juego

--- js/joc.js
+++ js/joc.js
@@ -1,87 +1,67 @@
-class Joc {
-    constructor(canvas, ctx) {
-        this.canvas = canvas; // el canvas on es dibuixa el joc
-        this.ctx = ctx; // context del canvas per dibuixar
-        this.amplada = canvas.width; // amplada del canvas
-        this.alcada = canvas.height; // alcada del canvas
-        this.bola = new Bola(new Punt(this.canvas.width / 2, this.canvas.height / 2), 3); // crea una bola al centre
-        this.pala = new Pala(new Punt((this.canvas.width - 60) / 2, this.canvas.height - 15), 60, 4); // crea la pala
-        this.totxo = new Totxo(new Punt((this.canvas.width - 120) / 2, (this.canvas.height - 20) / 3), 120, 20, "#0ad"); // crea un totxo
-        this.mur = new Mur(ctx, 0); // inicia un mur amb nivell 0
-        this.key = { // controladors de tecles
-            LEFT: {code: 37, pressed: false},
-            RIGHT: {code: 39, pressed: false},
+/*
+* CLASSE JOC
+*/
+
+class Joc{
+    constructor(canvas,ctx) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.amplada = canvas.width;
+        this.alcada = canvas.height;
+        this.totxoamplada = 22;
+        this.totxoalcada = 10; // MIDES DEL TOTXO EN PÍXELS
+        this.totxocolor = 20;
+
+        this.bola = new Bola(new Punt(this.canvas.width/2,this.canvas.height/2),3);
+        this.pala = new Pala(new Punt((this.canvas.width-60)/2,this.canvas.height-15),60,4);
+        this.totxo = new Totxo(new Punt((this.canvas.width-120)/2,(this.canvas.height-20)/3), 120, 20, "#0ad");  // només posem un totxo gegant
+
+        this.key = {
+            LEFT:{code:37, pressed:false},
+            RIGHT:{code:39, pressed:false},
             A: {code: 65, pressed: false},
             D: {code: 68, pressed: false}
-        };
-        this.jugando = false; // estat del joc
+        };      
     }
 
-    inicialitza() {
-        // gestiona els events de teclat
-        $(document).on("keydown", {joc: this}, function(e) {
-            var key = e.which;
-            var self = e.data.joc;
-            // actua segons la tecla premuda
-            if (key === self.key.LEFT.code || key === self.key.A.code) {
-                self.key.LEFT.pressed = true;
-                self.key.A.pressed = true;
-            } else if (key === self.key.RIGHT.code || key === self.key.D.code) {
-                self.key.RIGHT.pressed = true;
-                self.key.D.pressed = true;
+    draw(){
+        this.clearCanvas();
+        this.pala.draw(this.ctx);
+        this.bola.draw(this.ctx);
+        this.totxo.draw(this.ctx);
+    }
+
+    clearCanvas(){
+        this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height)
+    }
+
+    inicialitza(){
+        this.pala.draw(this.ctx);
+        this.bola.draw(this.ctx);
+        this.totxo.draw(this.ctx);
+        $(document).on("keydown", {joc:this}, function(e){
+            if (e.keyCode === joc.key.LEFT.code || e.keyCode === joc.key.A.code) {
+                joc.key.LEFT.pressed = true;
+                joc.key.A.pressed = true;
+            } else if (e.keyCode === joc.key.RIGHT.code || e.keyCode === joc.key.D.code) {
+                joc.key.RIGHT.pressed = true;
+                joc.key.D.pressed = true;
             }
         });
-
-        $(document).on("keyup", {joc: this}, function(e) {
-            var key = e.which;
-            var self = e.data.joc;
-            // actua quan es deixa de prémer la tecla
-            if (key === self.key.LEFT.code || key === self.key.A.code) {
-                self.key.LEFT.pressed = false;
-                self.key.A.pressed = false;
-            } else if (key === self.key.RIGHT.code || key === self.key.D.code) {
-                self.key.RIGHT.pressed = false;
-                self.key.D.pressed = false;
+        $(document).on("keyup", {joc:this}, function(e){
+            if (e.keyCode === joc.key.LEFT.code || e.keyCode === joc.key.A.code) {
+                joc.key.LEFT.pressed = false;
+                joc.key.A.pressed = false;
+            } else if (e.keyCode === joc.key.RIGHT.code || e.keyCode === joc.key.D.code) {
+                joc.key.RIGHT.pressed = false;
+                joc.key.D.pressed = false;
             }
         });
-
-        this.jugando = true;
-        this.bucleJuego(); // comença el bucle de joc
     }
 
-    bucleJuego() {
-        if (this.jugando) {
-            this.update(); // actualitza l'estat del joc
-            this.draw(); // dibuixa els elements del joc
-            requestAnimationFrame(this.bucleJuego.bind(this)); // continua el bucle
-        }
-    }
-
-    update() {
-        // actualitza la posició de la pala segons les tecles premudes
-        if (this.key.LEFT.pressed || this.key.A.pressed) {
-            this.pala.posicio.x -= this.pala.vx;
-            if (this.pala.posicio.x < 0) {
-                this.pala.posicio.x = 0;
-            }
-        }
-        if (this.key.RIGHT.pressed || this.key.D.pressed) {
-            this.pala.posicio.x += this.pala.vx;
-            if (this.pala.posicio.x > this.canvas.width - this.pala.amplada) {
-                this.pala.posicio.x = this.canvas.width - this.pala.amplada;
-            }
-        }
-    }
-
-    draw() {
-        this.clearCanvas(); // neteja el canvas
-        this.pala.draw(this.ctx); // dibuixa la pala
-        this.bola.draw(this.ctx); // dibuixa la bola
-        this.totxo.draw(this.ctx); // dibuixa el totxo
-        this.mur.draw(); // dibuixa el mur
-    }
-
-    clearCanvas() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // esborra tot el contingut del canvas
+    update(){
+        this.bola.update();
+        this.pala.update();
+        this.draw();
     }
 }
--- js/pala.js
+++ js/pala.js
@@ -3,42 +3,39 @@
 */
 
 class Pala {
-    
     constructor(puntPosicio, amplada, alcada){      
-        this.amplada = amplada; // amplada de la pala
-        this.alcada = alcada; // alcada de la pala
-        this.posicio = puntPosicio; // posició inicial de la pala en el canvas
-        this.vx = 2; // velocitat de la pala en l'eix x
-        this.color = "#D30"; // color de la pala
-    }
-    
-    // Funció per actualitzar la posició de la pala
-    update(){
-        if(this.Key.LEFT.pressed){ // si la tecla esquerra està premuda
-            this.posicio.x -= this.vx; // mou la pala a l'esquerra
-            if(this.posicio.x < 0){ // si la pala surt del canvas, la posicionem al límit
-                this.posicio.x = 0;
-            }
-        }
-        if(this.Key.RIGHT.pressed){ // si la tecla dreta està premuda
-            this.posicio.x += this.vx; // mou la pala a la dreta
-            if(this.posicio.x > this.amplada){ // si la pala surt del canvas, la posicionem al límit
-                this.posicio.x = this.amplada;
-            }
-        }
-    }
-    
-    // Funció per dibuixar la pala en el context del canvas
-    draw(ctx) {
-        ctx.save(); // guarda l'estat actual del context
-        ctx.fillStyle = this.color; // estableix el color de la pala
-        ctx.fillRect(this.posicio.x, this.posicio.y, this.amplada, this.alcada); // dibuixa la pala com un rectangle
-        ctx.restore(); // recupera l'estat anterior del context
+        this.amplada = amplada;
+        this.alcada = alcada;
+        this.posicio = puntPosicio;
+        this.vy = 0;     
+        this.vx = 2; // velocitat = 10 píxels per fotograma
+        this.color = "#D30"; 
     }
 
-    // Funció per moure la pala
-    mou(x, y){
-        this.posicio.x += x; // mou la pala en l'eix x
-        this.posicio.y += y; // mou la pala en l'eix y
+    update(){
+        if (joc.key.LEFT.pressed || joc.key.RIGHT.pressed) {
+            if (joc.key.LEFT.pressed) {
+                this.mou(-1*this.vx, this.vy)
+            } else {
+                this.mou(this.vx, this.vy)
+            }
+        }
+    }
+   
+    draw(ctx) {
+        ctx.save();
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.posicio.x, this.posicio.y, this.amplada, this.alcada);
+        ctx.restore();
+    }
+
+    mou(x,y){
+        console.log(this.posicio)
+        if (this.posicio.x >= 0 /* LEFT */ && this.posicio.x <= joc.canvas.width - this.amplada /* RIGHT */) {
+            this.posicio.x += x;
+            if (this.posicio.x === -1*this.vx) this.posicio.x = 0;
+            if (this.posicio.x === joc.canvas.width - this.amplada + this.vx) this.posicio.x = joc.canvas.width - this.amplada;
+        }
+        this.posicio.y += y;
     }
 }
