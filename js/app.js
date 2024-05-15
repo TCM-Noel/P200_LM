3b247bc687e7b71d23935ce636939770ba5f51eb
AlexBCX <alexxbc1998@gmail.com>
2024-05-15 06:21:08 +0200

    he creado la parte de modalidades de juego. este es el 1r commit. en principio hare otro para diferenciar los tipos de modalidades.

--- js/app.js
+++ js/app.js
@@ -7,15 +7,33 @@ $(document).ready(function() {
     let myCanvas = document.getElementById("joc");
     let ctx = myCanvas.getContext("2d");
 
-    joc = new Joc(myCanvas,ctx);
-    
+    joc = new Joc(myCanvas, ctx);
+
     $('#startButton').click(function() {
-        $('#menu').hide(); // Oculta el menú
-        $('#principal').show(); // Muestra el canvas
-        joc.inicialitza();
-        animacio();
+        $('#startButton').hide(); // Oculta el botón de inicio
+        $('#modalidades').show(); // Muestra las modalidades
     });
 
+    // Eventos para los botones de modalidad
+    $('#modo1').click(function() {
+        iniciarJuego('modalidad1');
+    });
+
+    $('#modo2').click(function() {
+        iniciarJuego('modalidad2');
+    });
+
+    $('#modo3').click(function() {
+        iniciarJuego('modalidad3');
+    });
+
+    function iniciarJuego(modalidad) {
+        console.log(modalidad + " seleccionada");
+        $('#menu').hide(); // Oculta el menú
+        $('#principal').show(); // Muestra el canvas
+        joc.inicialitza(modalidad); // Inicializa el juego con la modalidad seleccionada
+        animacio();
+    }
 });
 
 function animacio() {
